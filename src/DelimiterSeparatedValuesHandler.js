adca5262bf12d939ff6a991dd298b9e7299c703f
Gilberto Augusto <gilberto.bastos@outlook.com.br>
2020-05-28 20:47:13 -0300

    Initial commit

---
+++ src/DelimiterSeparatedValuesHandler.js
@@ -0,0 +1,33 @@
+class DelimiterSeparatedValuesHandler {
+
+    #delimiterSymbol;
+    #newLineSymbol;
+    #columnStructureArray;
+    #escapeSymbol;
+
+    constructor(delimiterSymbol, newLineSymbol, columnStructureArray, escapeSymbol = null) {
+        this.#delimiterSymbol = delimiterSymbol;
+        this.#newLineSymbol = newLineSymbol;
+        this.#columnStructureArray = columnStructureArray;
+        this.#escapeSymbol = escapeSymbol;
+    }
+
+    covertDSVTextToObject(text, hasHeader = false) {
+        
+    }
+
+    convertObjectToDSVText(object, printHeader = true) {
+
+    }
+}
+
+class DSVColumnStructure {
+
+    columnName;
+    columnType;
+
+    constructor(columnName, columnType) {
+        this.columnName = columnName;
+        this.columnType = columnType;
+    }
+}
---
+++ src/FixedLenghtFormatHandler.js
@@ -0,0 +1,32 @@
+class FixedLenghtFormatHandler {
+
+
+    convertFixedLengthFmtTextToObj(text) {
+
+    }
+
+    convertObjToFixedLengthFmtText (object) {
+
+    }
+
+
+}
+
+class FixedLenghtRowTypeStructure {
+
+    rowTypeFlag;
+    posRowTypeFlag;
+    fixedLengthColumnStructureArray;
+
+    constructor(rowTypeFlag, posRowTypeFlag, fixedLengthColumnStructureArray) {
+        this.rowTypeFlag = rowTypeFlag;
+        this.posRowTypeFlag = posRowTypeFlag;
+        this.fixedLengthColumnStructureArray = fixedLengthColumnStructureArray;
+    }
+}
+
+class FixedLengthColumnStructure {
+
+    
+
+}
