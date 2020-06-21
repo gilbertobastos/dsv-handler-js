99b4e696a26a8b9f86f7f664177c61eaef2837fd
Gilberto Augusto <gilberto.bastos@outlook.com.br>
2020-06-20 21:51:47 -0300

    Updated the README and the test file.

--- src/app_test.js
+++ src/app_test.js
@@ -5,7 +5,17 @@ let dsvHandler = new DelimiterSeparatedValuesHandler(';', '\n', '"');
 let dsvTextGoodArtists = 
     'Col1;Col2;Col3;Col4\n' +
     'RDP;Voivod;Metallica;Sepultura\n' +
-    'Ivan Lins;Gilberto Gil;Olivia Byington;Flávio Venturini';
+    'Ivan Lins;Gilberto Gil;"Olivia;;;Byington";Flávio Venturini';
 
-let objGoodArtists = dsvHandler.covertDSVTextToObject(dsvTextGoodArtists);
-console.log(objGoodArtists);
+let objectGoodDrummers = 
+    [
+        ['Col1', 'Col2', 'Col3'],
+        ['Billy Cobham', 'Bill Bruford', 'Alan White'],
+        ['Carl Palmer', 'Neil Peart', 'Peter;;;Erskine']  
+    ];
+
+console.log('Parsing the text from a DSV to object:');
+console.log(dsvHandler.covertDSVTextToObject(dsvTextGoodArtists));
+
+console.log('Bulding a DSV text from a object:');
+console.log(dsvHandler.convertObjectToDSVText(objectGoodDrummers));
